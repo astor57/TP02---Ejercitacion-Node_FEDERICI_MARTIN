e2004b2c3867464494385ab3f835dbf2b000c645
astor57 <astorfederici02@gmail.com>
2025-03-29 16:10:43 -0300

    Ejercicios 4, 5 y 6

---
+++ src/modules/ej04.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
+const ARCHIVO_SALIDA = "./archivo-salida.txt";
+
+console.clear();
+copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+
+function copiar(origen, destino) {
+
+    fs.readFile(origen, 'utf8', (error, info) => {
+        if (err) {
+            console.log(`Error al leer el archivo: ${error}`);
+            return;
+        }
+        fs.writeFile(destino, info, 'utf8', (error) => {
+            if (error) {
+                console.log(`Error al escribir el archivo: ${error}`);
+                return;
+            }
+            console.log(`El archivo ha sido copiado de '${origen}' a '${destino}'`);
+        });
+    });
+}
